6c1f94968fe1808e04d9c98fdb3d818f31d56d50
Daniel Lim <daniellim@Daniels-MBP.home>
2022-09-05 12:07:25 -0700

    added sql scripts for creating 'movies' and 'popularities' tables and seeding the data

---
+++ sql/seeders/insert.movies.sql
@@ -0,0 +1,6 @@
+INSERT INTO movies (title, description, released, duration, rating)
+VALUES
+('Music', "Zu is newly sober when she receives news that she is to become the sole guardian of her half-sister named Music, a young girl on the autism spectrum. The film explores two of Sia's favorite themes: finding your voice and creating ...", '2021-2-12', 107 ,'PG-13'),
+('Starwars: Goretech', 'N/A', '2018-12-07', 90, 'N/A'),
+('Blade Runner', 'A blade runner must pursue and terminate four replicants who stole a ship in space, and have returned to Earth to find their creator.', '1982-1-25', 117, 'R'),
+('The Lord of the Rings: The Fellowship of the Ring', 'A meek Hobbit from the Shire and eight companions set out on a journey to destroy the powerful One Ring and save Middle-earth from the Dark Lord Sauron.', '2001-12-19', 178, 'PG-13');
---
+++ sql/seeders/insert.popularities.sql
@@ -0,0 +1,5 @@
+INSERT INTO popularities (likes, dislikes, movie_id)
+VALUES
+(0,0,1),
+(1,0,2),
+(0,1,3);
