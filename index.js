c3c19df01d5b4941202d60ab84f41b62e8869b7a
Daniel Lim <daniellim@Daniels-MBP.home>
2022-09-05 11:04:09 -0700

    main index.js setup - configuration, express app running

---
+++ index.js
@@ -0,0 +1,25 @@
+// Import Express
+const express = require("express");
+// Instantiate an Express app
+const app = express();
+// Set the port to use
+const port = 3000;
+
+// Middlewares
+let cors = require("cors");
+
+// Cors
+app.use(cors());
+app.options("*", cors());
+
+// Bodyparser
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.get("/", (req, res) => {
+  res.send("hello ensemble");
+});
+
+app.listen(port, () => {
+  console.log(`Server is successfully running on port ${port}`);
+});
