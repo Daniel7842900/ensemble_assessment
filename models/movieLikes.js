99d2fab67623c31793b365b2e3644831ff992ecd
Daniel Lim <daniellim@Daniels-MBP.home>
2022-09-05 23:23:54 -0700

    Implemented movieLikes service and model

--- models/movieLikes.js
+++ models/movieLikes.js
@@ -8,5 +8,49 @@ class MovieLikes {
     this.movie_id = movie_id;
   }
 
-  updateLikesById = async (id, type, result) => {};
+  updateLikesById = async (id, type, result) => {
+    const findMovieLikesQuery = "SELECT * FROM movie_likes WHERE movie_id = ?";
+    const dbType = type + "d";
+    const updateMovieLikesQuery =
+      "UPDATE movie_likes SET " + dbType + " = ? WHERE movie_id = ?";
+
+    try {
+      let rows = await db.query(findMovieLikesQuery, id);
+      let row = rows[0][0];
+      if (type === "like") {
+        if (row.liked != row.disliked && !row.liked) {
+          // Case: liked = 0 disliked = 1
+          return result({ equal: true }, null);
+        } else if (row.liked) {
+          // Case: liked = 1 disliked = 0
+          let updatedRows = await db.query(updateMovieLikesQuery, [0, id]);
+          return result(null, updatedRows);
+        } else {
+          // Case: liked = 0 disliked = 0
+          let updatedRows = await db.query(updateMovieLikesQuery, [1, id]);
+          return result(null, updatedRows);
+        }
+      } else if (type === "dislike") {
+        if (row.liked != row.disliked && !row.disliked) {
+          // Case: liked = 1 disliked = 0
+          return result({ equal: true }, null);
+        } else if (row.disliked) {
+          // Case: liked = 0 disliked = 1
+          let updatedRows = await db.query(updateMovieLikesQuery, [0, id]);
+          return result(null, updatedRows);
+        } else {
+          // Case: liked = 0 disliked = 0
+          let updatedRows = await db.query(updateMovieLikesQuery, [1, id]);
+          return result(null, updatedRows);
+        }
+      } else {
+        return result({ invalidQueryParam: true }, null);
+      }
+    } catch (err) {
+      console.log("movie_likes updateLikeById err: " + err);
+      return result(err, null);
+    }
+  };
 }
+
+module.exports = MovieLikes;
--- services/movieLikes.js
+++ services/movieLikes.js
@@ -4,5 +4,38 @@ const Joi = require("joi");
 const movieLikes = new MovieLikes();
 
 exports.updateLikesById = (req, res) => {
-  movieLikes.updateLikesById(req.params.id, type, async (err, data) => {});
+  let type = req.query.type;
+  // Check if 'type' query parameter
+  if (type === undefined) {
+    res.status(404).send({
+      message: `Wrong query parameter!`,
+    });
+  }
+
+  // Check query parameter
+  if (type.length === 0) {
+    res.status(404).send({
+      message: `Query parameter needs to be provided!`,
+    });
+  }
+
+  movieLikes.updateLikesById(req.params.id, type, async (err, data) => {
+    if (err) {
+      if (err.equal) {
+        res.status(404).send({
+          message: `You can't like and dislike at the same time!`,
+        });
+      } else if (err.invalidQueryParam) {
+        res.status(404).send({
+          message: `Invalid query parameter`,
+        });
+      } else {
+        res.status(500).send({
+          message: err.message,
+        });
+      }
+    } else {
+      //   res.send(data);
+    }
+  });
 };
